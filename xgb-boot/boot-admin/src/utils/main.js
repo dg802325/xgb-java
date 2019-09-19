379c6c5375d386784a481482f4c5f51e7bd22501
denggen <email>
2019-09-19 09:28:39 +0800

    update

--- xgb-boot/boot-admin/src/utils/main.js
+++ xgb-boot/boot-admin/src/utils/main.js
@@ -1,8 +1,6 @@
 import store from "../store";
 
 function checkPermission(key) {
-    console.log(store.getters.permissions)
-    console.log(key)
     return store.getters.permissions.indexOf(key)>= 0;
 }
 
