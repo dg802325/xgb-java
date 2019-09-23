aef337f7ca75db7e6ed9e84f56f3273e61565915
denggen <email>
2019-09-23 18:43:40 +0800

    update

--- xgb-common/src/main/java/com/xgb/utils/MD5Util.java
+++ xgb-common/src/main/java/com/xgb/utils/MD5Util.java
@@ -1,9 +1,13 @@
 package com.xgb.utils;
 
 
-import java.io.UnsupportedEncodingException;
-import java.security.MessageDigest;
+import org.apache.commons.codec.binary.Base64;
+
+import javax.crypto.Cipher;
+import javax.crypto.KeyGenerator;
+import java.security.Key;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 
 /**
  * MD5加密类（封装jdk自带的md5加密方法）
@@ -11,31 +15,120 @@ import java.security.NoSuchAlgorithmException;
  */
 public class MD5Util {
 
-    public static String toMd5(String source) {
+    public static Key DEFAULT_KEY = null;
+
+    public static final String DEFAULT_SECRET_KEY = "xgb";
+
+    public static final String DES = "DES";
+
+    static {
+        DEFAULT_KEY = obtainKey(DEFAULT_SECRET_KEY);
+    }
+
+    /**
+     * 获得key
+     **/
+    public static Key obtainKey(String key) {
+        if (key == null) {
+            return DEFAULT_KEY;
+        }
+        KeyGenerator generator = null;
         try {
-            return encodeMd5(source.getBytes("UTF-8"));
-        } catch (UnsupportedEncodingException e) {
+            generator = KeyGenerator.getInstance(DES);
+        } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
-        return null;
+        generator.init(new SecureRandom(key.getBytes()));
+        Key key1 = generator.generateKey();
+        generator = null;
+        return key1;
     }
 
-    private static String encodeMd5(byte[] source) {
+    /**
+     * 加密<br>
+     * String明文输入,String密文输出
+     */
+    public static String toMd5(String str) {
+        return encode(null, str);
+    }
+
+    /**
+     * 加密<br>
+     * String明文输入,String密文输出
+     */
+    public static String encode(String key, String str) {
+        return Base64.encodeBase64URLSafeString(obtainEncode(key, str.getBytes()));
+        // return Hex.encodeHexString(obtainEncode(key, str.getBytes()));
+        // 可以转化为16进制数据
+    }
+
+    /**
+     * 解密<br>
+     * 以String密文输入,String明文输出
+     */
+    public static String md5Decrypt(String str) {
+        return decode(null, str);
+    }
+
+    /**
+     * 解密<br>
+     * 以String密文输入,String明文输出
+     */
+    public static String decode(String key, String str) {
+        return new String(obtainDecode(key, Base64.decodeBase64(str)));
+        // 可以转化为16进制的数据
+//      try {
+//          return new String(obtainDecode(key, Hex.decodeHex(str.toCharArray())));
+//      } catch (DecoderException e) {
+//          // TODO Auto-generated catch block
+//          e.printStackTrace();
+//      }
+    }
+
+    /**
+     * 加密<br>
+     * 以byte[]明文输入,byte[]密文输出
+     */
+    private static byte[] obtainEncode(String key, byte[] str) {
+        byte[] byteFina = null;
+        Cipher cipher;
         try {
-            return encodeHex(MessageDigest.getInstance("MD5").digest(source));
-        } catch (NoSuchAlgorithmException e) {
-            throw new IllegalStateException(e.getMessage(), e);
+            Key key1 = obtainKey(key);
+            cipher = Cipher.getInstance(DES);
+            cipher.init(Cipher.ENCRYPT_MODE, key1);
+            byteFina = cipher.doFinal(str);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            cipher = null;
         }
+        return byteFina;
     }
 
-    private static String encodeHex(byte[] bytes) {
-        StringBuffer buffer = new StringBuffer(bytes.length * 2);
-        for (int i = 0; i < bytes.length; i++) {
-            if (((int) bytes[i] & 0xff) < 0x10)
-                buffer.append("0");
-            buffer.append(Long.toString((int) bytes[i] & 0xff, 16));
-        }
-        return buffer.toString();
+    @Override
+    public int hashCode() {
+        return super.hashCode();
     }
+
+    /**
+     * 解密<br>
+     * 以byte[]密文输入,以byte[]明文输出
+     */
+    private static byte[] obtainDecode(String key, byte[] str) {
+        Cipher cipher;
+        byte[] byteFina = null;
+        try {
+            Key key1 = obtainKey(key);
+            cipher = Cipher.getInstance(DES);
+            cipher.init(Cipher.DECRYPT_MODE, key1);
+            byteFina = cipher.doFinal(str);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            cipher = null;
+        }
+        return byteFina;
+    }
+
 
 }
--- xgb-test/src/main/java/com/xgb/iterator/test.java
+++ xgb-test/src/main/java/com/xgb/iterator/test.java
@@ -1,68 +1,56 @@
 package com.xgb.iterator;
 
+import java.security.MessageDigest;
 import java.util.ArrayList;
 import java.util.Iterator;
 
 public class test {
 
-    public static void main(String[] args) {
-        ArrayList<Person> list = new ArrayList<Person>();
-        list.add(new Person("张三",2));
-        list.add(new Person("李四",5));
-        list.add(new Person("王五",9));
-        Iterator<Person> iterator = list.iterator();
-        while (iterator.hasNext()){
-            Person next = iterator.next();
-            next.setAge(6);
-            System.out.println(next);
+    /***
+     * MD5加码 生成32位md5码
+     */
+    public static String string2MD5(String inStr) {
+        MessageDigest md5 = null;
+        try {
+            md5 = MessageDigest.getInstance("MD5");
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            e.printStackTrace();
+            return "";
         }
-//        while (iterator.hasNext()){
-//            newTitle title = (newTitle) iterator.next();
-//        }
-//        Collections.fill(list,"东方不败");
-        Iterator<Person> iterator1 = list.iterator();
-        System.out.println("修改后");
-        while (iterator1.hasNext()){
-            Person next = iterator1.next();
-            System.out.println(next);
+        char[] charArray = inStr.toCharArray();
+        byte[] byteArray = new byte[charArray.length];
+        for (int i = 0; i < charArray.length; i++)
+            byteArray[i] = (byte) charArray[i];
+        byte[] md5Bytes = md5.digest(byteArray);
+        StringBuffer hexValue = new StringBuffer();
+        for (int i = 0; i < md5Bytes.length; i++) {
+            int val = ((int) md5Bytes[i]) & 0xff;
+            if (val < 16)
+                hexValue.append("0");
+            hexValue.append(Integer.toHexString(val));
         }
+        return hexValue.toString();
     }
 
-
-    static class Person{
-        private String name;
-        private int age;
-
-        public String getName() {
-            return name;
+    /**
+     * 加密解密算法 执行一次加密，两次解密
+     */
+    public static String convertMD5(String inStr) {
+        char[] a = inStr.toCharArray();
+        for (int i = 0; i < a.length; i++) {
+            a[i] = (char) (a[i] ^ 't');
         }
+        String s = new String(a);
+        return s;
+    }
 
-        public void setName(String name) {
-            this.name = name;
-        }
-
-        public int getAge() {
-            return age;
-        }
-
-        public void setAge(int age) {
-            this.age = age;
-        }
-
-        public Person(String name, int age) {
-            this.name = name;
-            this.age = age;
-        }
-
-        public Person() {
-        }
-
-        @Override
-        public String toString() {
-            return "Person{" +
-                    "name='" + name + '\'' +
-                    ", age=" + age +
-                    '}';
-        }
+    // 测试主函数
+    public static void main(String args[]) {
+        String s = new String("123456");
+        System.out.println("原始：" + s);
+        System.out.println("MD5后：" + string2MD5(s));
+        System.out.println("加密的：" + convertMD5(s));
+        System.out.println("解密的：" + convertMD5(convertMD5(s)));
     }
 }
