a6e9eb0a01fbe4eb11540a8b409979ec61cfcac1
Mr Deng <295841112@qq.com>
2019-12-11 14:00:03 +0800

    remove directory from reptile repository

--- xgb-reptile/spider1/src/main/java/com/xgb/emnu/TaskLevel.java
+++
@@ -1,9 +0,0 @@
-package com.xgb.emnu;
-
-/**
- * 抓取任务的level级别
- */
-public enum TaskLevel {
-
-    HIGH,MIDDLE,LOW
-}
--- xgb-reptile/spider1/src/main/java/com/xgb/service/ICrawler.java
+++
@@ -1,8 +0,0 @@
-package com.xgb.service;
-
-import com.xgb.pojo.CrawlResultPojo;
-import com.xgb.pojo.UrlPojo;
-
-public interface ICrawler {
-	public CrawlResultPojo crawl(UrlPojo urlPojo);
-}
